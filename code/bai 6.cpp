64cdc354d95295af17862b3bcdda3b75b1481f22
Thai <trinhhoangthai2005@gmail.com>
2024-08-03 19:16:48 +0700

    buoi 5 phan 2 bai 6

---
+++ code/bai 6.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+
+int demSoChuSo(int n) {
+    int count = 0;
+    while (n != 0) {
+        n /= 10; 
+        count++; 
+    }
+    return count;
+}
+
+int main() {
+    int n;
+    printf("Nhap so nguyen duong n: ");
+    scanf_s("%d", &n);
+    if (n <= 0) {
+        printf("Vui long nhap mot so nguyen duong.\n");
+        return 1;
+    }
+    printf("So chu so cua %d la: %d\n", n, demSoChuSo(n));
+    return 0;
+}
