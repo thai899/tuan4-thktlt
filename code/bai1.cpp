c589c962e771be2c75d01b6efa6b70827cd3461c
Thai <trinhhoangthai2005@gmail.com>
2024-08-03 18:44:23 +0700

    buoi 5 phan 2 bai 1.4

--- code/bai1.cpp
+++ code/bai1.cpp
@@ -4,7 +4,7 @@ void menu()
 	printf("\n1.1+2+....+n");
 	printf("\n2.1/2 +2/3 + ...+ n/n+1");
 	printf("\n3.1+1/3+...+1/(2n+1)");
-	printf("\n4.1*2+2*3+...+n*n+1");
+	printf("\n4.1*2+2*3+...+n*(n+1)");
 }
 int tong(int n)
 {
@@ -27,6 +27,14 @@ float tong1(int n)
 	else
 		return (float)1 / (2 * n + 1) + tong(n - 1);
 }
+float tong2(int n)
+{
+	if (n == 1)
+		return 2;
+	else
+		return n * (n + 1) + tong(n - 1);
+}
+
 int main()
 {
 	int lc, n;
@@ -50,10 +58,15 @@ int main()
 		}break;
 		case 3:
 		{
-			int t = 1.67;
 			printf("Nhap vao n\n ");
 			scanf_s("%d", &n);
 			printf("1+1/3+...+1/(2*%d+1) = %0.2f \n", n, tong1(n));
+		}break;
+		case 4:
+		{
+			printf("Nhap vao n\n ");
+			scanf_s("%d", &n);
+			printf("1*2+2*3+...+%d*(%d+1) = %0.2f \n", n, n, tong2(n));
 		}break;
 		}
 	} while (lc < 5);
