91a7a1ad62419f5decb8cf931998d552dca68c7d
Thai <trinhhoangthai2005@gmail.com>
2024-08-03 19:21:09 +0700

    buoi 5 phan 2 bai 7

---
+++ code/bai 7.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+
+int Fibonacci(int n) {
+    if (n <= 2) return 1; 
+    return Fibonacci(n - 1) + Fibonacci(n - 2); 
+}
+
+int main() {
+    int n;
+    printf("Nhap so nguyen duong n: ");
+    scanf_s("%d", &n);
+    printf("So Fibonacci thu %d la: %d\n", n, Fibonacci(n));
+    return 0;
+}
